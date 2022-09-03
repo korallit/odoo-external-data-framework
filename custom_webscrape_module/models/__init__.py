3a63c88da7f12573e0f9a7e63996c64c54ce6945
Janos Gerzson <gerzsonj@gmail.com>
2022-09-03 20:59:46 +0200

    prune repo

--- custom_webscrape_module/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# from . import webscrape
-# from . import meleget_hu
--- meleget_hu/models/product_template.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-
-from odoo import fields, models
-
-
-class KlimatszeretnekProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    description_sale_html = fields.Html("Sales Description HTML")
