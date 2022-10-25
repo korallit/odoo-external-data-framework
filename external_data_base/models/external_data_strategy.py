4b66744ff638d119486178d30865cb7046512320
grzs <gerzsonj@gmail.com>
2022-10-25 17:17:18 +0200

    [FIX] vals or something_conditional()

--- external_data_base/models/external_data_strategy.py
+++ external_data_base/models/external_data_strategy.py
@@ -306,10 +306,10 @@ class ExternalDataStrategy(models.Model):
                 metadata.pop('drop')
                 return True
             self._prune_vals(vals, **metadata)
-            vals and metadata['external_objects'].sanitize_values(
-                vals, **metadata)
-            if vals and not field_mapping.skip_write:
-                metadata['record'].write(vals)
+            if vals:
+                metadata['external_objects'].sanitize_values(vals, **metadata)
+                if not field_mapping.skip_write:
+                    metadata['record'].write(vals)
 
     @api.model
     def _pull(self, field_mapping, data, metadata):
