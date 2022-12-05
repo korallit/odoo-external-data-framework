ebe4d0d632c754bb8dea7ed25b6aae8910fbab0b
grzs <gerzsonj@gmail.com>
2022-12-05 19:09:38 +0100

    [UPD] jinja renderer passed values selected by key

--- external_data_serializer_jinja2/models/external_data_serializer_jinja2.py
+++ external_data_serializer_jinja2/models/external_data_serializer_jinja2.py
@@ -12,8 +12,11 @@ class ExternalDataSerializer(models.Model):
     )
     jinja2_template = fields.Text("Jinja2 template")
 
-    def _render_jinja2(self, data, metadata, key=False):
+    def _render_jinja2(self, data, metadata, key='items'):
         self.ensure_one()
+        if key:
+            data = data.get(key)
+
         environment = jinja2.Environment()
         template = environment.from_string(self.jinja2_template)
-        return template.render(vals=data, metadata=metadata)
+        return template.render(items=data, metadata=metadata)
