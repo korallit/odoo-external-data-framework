a95a5ff99d5f2d632bb325b00f0220358ea47b32
Janos Gerzson <gerzsonj@gmail.com>
2022-12-06 08:57:34 +0100

    [UPD] pagination option on strategy model, view

--- external_data_base/models/external_data_strategy.py
+++ external_data_base/models/external_data_strategy.py
@@ -63,6 +63,7 @@ class ExternalDataStrategy(models.Model):
         domain="[('data_source_id', '=', data_source_id)]",
     )
     batch_size = fields.Integer("Batch size", default=10)
+    offset = fields.Integer("Page", help="offset", default=0)
     exposed = fields.Boolean("Exposed to REST")
     export_filename = fields.Char(
         "Export filename",
@@ -91,7 +92,15 @@ class ExternalDataStrategy(models.Model):
                 record.export_filename,
             ]
             if all(path_parts):
-                record.export_url = '/'.join(path_parts)
+                url = '/'.join(path_parts)
+                params = []
+                if record.batch_size:
+                    params.append("page_size=" + str(record.batch_size))
+                if record.offset:
+                    params.append("page=" + str(record.offset))
+                if params:
+                    url += "?" + '&'.join(params)
+                record.export_url = url
 
     def button_details(self):
         self.ensure_one()
