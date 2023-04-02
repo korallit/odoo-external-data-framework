4d0f91c00e57b644a21305406a741f505fba44c8
Janos Gerzson <gerzsonj@gmail.com>
2023-04-02 14:41:24 +0200

    [IMP} worker splitter and funnel logic

--- external_data_base/models/external_data_worker.py
+++ external_data_base/models/external_data_worker.py
@@ -1,5 +1,5 @@
 from odoo import api, fields, models
-from odoo.exceptions import MissingError, UserError
+from odoo.exceptions import MissingError, UserError, ValidationError
 
 
 class ExternalDataWorker(models.AbstractModel):
@@ -7,6 +7,16 @@ class ExternalDataWorker(models.AbstractModel):
     _description = "External Data Worker Template"
 
     name = fields.Char(required=True)
+    forward_type = fields.Selection(
+        "Forward type",
+        selection=[
+            ('simple', "simple"),
+            ('spltter', "splitter"),
+            ('funnel', "funnel"),
+        ],
+        default='simple',
+        required=True,
+    )
     batch_size = fields.Integer("Batch size", default=0)
     signature = fields.Char(compute='_compute_signature')
 
@@ -14,8 +24,83 @@ class ExternalDataWorker(models.AbstractModel):
         for record in self:
             record.signature = f"{record._name}:{record.id}"
 
-    def process(self, data, metadata={}):
+    def process(self, data=None, metadata={}):
+        for worker in self:
+            processed_data = worker._process(data, metadata)
+            if worker.forward_type == 'funnel':
+                return worker._process_funnel(processed_data)
+            return processed_data
+
+    def _process_funnel(self, data):
+        self.ensure_one()
+        funnel = self.env['external.data.funnel']
+        if funnel.count_items(self.signature) < self.batch_size - 1:
+            if not isinstance(data, str) and hasattr(self, '_data_to_str'):
+                data = self._data_to_str(data)
+            funnel.add(self.signature, data)
+            return None
+        else:
+            dataset = funnel.release(self.signature)
+            if hasattr(self, '_data_from_str'):
+                dataset = [
+                    self._data_from_str(data_str)
+                    for data_str in dataset
+                ]
+            dataset.append(data)
+            if hasattr(self, '_compile_dataset'):
+                dataset = self._compile_dataset(dataset)
+            return dataset
+
+    def _process(self, data, metadata):
+        self.ensure_one()
+        # worker specific logic
         return data
+
+    # def _data_to_str(self, data):
+    #     self.ensure_one()
+    #     # worker specific logic
+    #     return str(data)
+
+    # def _data_from_str(self, data):
+    #     self.ensure_one()
+    #     # worker specific logic
+    #     return data
+
+    # def _compile_dataset(self, dataset):
+    #     self.ensure_one()
+    #     # worker specific logic
+    #     return dataset
+
+
+class ExternalDataFunnel(models.TransientModel):
+    _name = 'external.data.funnel'
+    _description = "Worker Funnel"
+
+    worker_signature = fields.Integer(required=True)
+    data = fields.Text(required=True)
+
+    @api.model
+    def count_items(model, signature):
+        return model.search_count([('worker_signature', '=', signature)])
+
+    @api.model
+    def add(model, signature, data):
+        if not isinstance(data, str):
+            raise ValidationError(
+                f"Only string can be added to funnel, got {type(data)}."
+                "worker._data_to_str(data) should be implemented!"
+            )
+        model.create([{
+            'worker_signature': signature,
+            'data': data,
+        }])
+
+    @api.model
+    def release(model, signature):
+        records = model.search([('worker_signature', '=', signature)])
+        dataset = [r.data for r in records]
+        records.unlink()
+        return dataset
 
 
 class ExternalDataSubscription(models.Model):
@@ -83,19 +168,29 @@ class ExternalDataSubscription(models.Model):
 
     @api.model
     def process_and_forward(model, signature, data=None, metadata={}):
-        data = model._process(signature, data, metadata)
-        for subscription in model._get_subscriptions(signature):
-            metadata_copy = metadata.copy()
-            if subscription.sync:
-                subscription._sync_forward(data, metadata_copy)
-            subscription.submit(data, metadata_copy)
+        data, forward_type = model._process(signature, data, metadata)
+        if data is None:  # maybe it's a waiting funnel
+            return
+        subscriptions = model._get_subscriptions(signature)
+        if forward_type == 'spliter' and hasattr(data, '__iter__'):
+            for d in data:
+                subscriptions._forward(d, metadata.copy())
+        else:
+            subscriptions._forward(data, metadata.copy())
 
     @api.model
     def _process(model, signature, data, metadata):
         worker = model._get_worker(signature)
         if not worker.exists():
             raise MissingError(f"No worker found for signature '{signature}'")
-        return worker.process(data, metadata)
+        return worker.process(data, metadata), worker.forward_type
+
+    def _forward(self, data, metadata):
+        for subscription in self:
+            if subscription.sync:
+                subscription._sync_forward(data, metadata)
+            else:
+                subscription.submit(data, metadata)
 
     def _sync_forward(self, data, metadata):
         self.ensure_one()
